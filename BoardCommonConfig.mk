e44a2bf5b6c34003b7160ae54b784b98e78fa7dc
OliverG96 <oliverarafo@gmail.com>
2013-08-28 22:30:00 +0100

    board: Delete useless defines

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -57,8 +57,6 @@ BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_NAME := "dhd"
-WIFI_BAND := 802_11_ABG
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
